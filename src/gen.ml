75026f6e4369fac80880eff848a9f8a2888cc9a9
malloc-nbytes <zdhdev@yahoo.com>
2024-01-31 19:24:44 -0600

    update

--- src/gen.ml
+++ src/gen.ml
@@ -2,7 +2,45 @@ module Gen = struct
   open Ast
   open Opcode
 
-  let generate_bytecode (program : Ast.program) : Opcode.byte list =
+  let evaluate_expr (expr : Ast.expr) : (Opcode.t * Opcode.byte) list =
+    match expr with
+    | Ast.Binary bin -> assert false
+    | Ast.Term Ast.Ident ident -> failwith "Ident Term expr not implemented"
+    | Ast.Term Ast.Intlit term ->
+       let value = int_of_string term.value in
+       [Opcode.IPush, value]
+
+  let evaluate_mut_stmt (stmt : Ast.mut_stmt) : Opcode.byte =
     assert false
+
+  let evaluate_let_stmt (stmt : Ast.let_stmt) : Opcode.byte =
+    assert false
+
+  let evaluate_block_stmt (stmt : Ast.block_stmt) : Opcode.byte list =
+    assert false
+
+  let evaluate_proc_def_stmt (stmt : Ast.proc_def_stmt) : Opcode.byte list =
+    assert false
+
+  let evaluate_stmt (stmt : Ast.stmt) : Opcode.byte list =
+    match stmt with
+    | Ast.Proc_def procdef -> assert false
+    | Ast.Block block -> assert false
+    | Ast.Let letstmt -> assert false
+    | Ast.Mut mutstmt -> assert false
+    | Ast.If ifstmt -> failwith "traversing if_stmt not implemented"
+    | Ast.While whilestmt -> failwith "traversing while_stmt not implemented"
+
+  let evaluate_toplvl_stmt (stmt : Ast.toplvl_stmt) : Opcode.byte list =
+    match stmt with
+    | Ast.Proc_def stmt' -> assert false
+    | Ast.Let stmt' -> assert false
+
+  let generate_bytecode (program : Ast.program) : Opcode.byte list =
+    let rec aux (stmts : Ast.toplvl_stmt list) : Opcode.byte list =
+      match stmts with
+      | [] -> []
+      | hd :: tl -> evaluate_toplvl_stmt hd @ aux tl in
+    aux program
 
 end
